616dc831096cf15b8f686af5d3adfd9a2fea5504
KnotEnvy <jwsnyder@gmail.com>
2023-07-02 19:22:26 -0400

    aider: Refactored database.js to use async/await and promises for better error handling and readability.
    # Context:
    USER: refactor our code as the game is not starting or contacting the api
    USER: I added these *read-write* files: database.js

--- database.js
+++ database.js
@@ -4,59 +4,72 @@ const { verbose } = sqlite3;
 const db = verbose();
 
 class Database {
-    constructor() {
-      this.connection = new sqlite3.Database('game.db');
-      this.createUserTable();
-    }
-  
-    createUserTable() {
-        const sql = `
-          CREATE TABLE IF NOT EXISTS users (
-            username TEXT PRIMARY KEY,
-            password TEXT,
-            level INTEGER,
-            experience INTEGER,
-            gold INTEGER,
-            items TEXT,
-            magic TEXT
-          )
-        `;
-        this.connection.run(sql);
-      }
-    
-      saveUser(user) {
-        const sql = `
-          INSERT OR REPLACE INTO users (username, password, level, experience, gold, items, magic)
-          VALUES (?, ?, ?, ?, ?, ?, ?)
-        `;
-        const params = [
-          user.username,
-          user.password,
-          user.level,
-          user.experience,
-          user.gold,
-          JSON.stringify(user.items),
-          JSON.stringify(user.magic)
-        ];
-        this.connection.run(sql, params);
-      }
-    
-      loadUser(username) {
-        const sql = `
-          SELECT * FROM users WHERE username = ?
-        `;
-        this.connection.get(sql, [username], (err, row) => {
-          if (err) {
-            return console.error(err.message);
-          }
-          if (row) {
-            console.log(row.username, row.password, row.level, row.experience, row.gold);
-            console.log(JSON.parse(row.items), JSON.parse(row.magic));
-          } else {
-            console.log(`No user found with the username ${username}`);
-          }
-        });
-      }
-    }
+  constructor() {
+    this.connection = new sqlite3.Database('game.db');
+  }
+
+  async createUserTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT,
+        level INTEGER,
+        experience INTEGER,
+        gold INTEGER,
+        items TEXT,
+        magic TEXT
+      )
+    `;
+    return new Promise((resolve, reject) => {
+      this.connection.run(sql, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async saveUser(user) {
+    const sql = `
+      INSERT OR REPLACE INTO users (username, password, level, experience, gold, items, magic)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+    const params = [
+      user.username,
+      user.password,
+      user.level,
+      user.experience,
+      user.gold,
+      JSON.stringify(user.items),
+      JSON.stringify(user.magic)
+    ];
+    return new Promise((resolve, reject) => {
+      this.connection.run(sql, params, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async loadUser(username) {
+    const sql = `
+      SELECT * FROM users WHERE username = ?
+    `;
+    return new Promise((resolve, reject) => {
+      this.connection.get(sql, [username], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+}
 
 export default Database;
